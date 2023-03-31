73b514586448c912dba76b20f389b1149b13a021
Dan Fedick <daniel.fedick@hashicorp.com>
2023-03-31 08:36:55 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# STIG Viewer
+
+This is a simple command-line tool to read and search through JSON data containing Security Technical Implementation Guide (STIG) information. It allows users to list all the IDs within the provided STIG data and search for findings with a specific STIG version. The program can read data from a local file or fetch data from a remote URL.
+
+This application was designed for the [Application STIG](https://www.stigviewer.com/stig/application_security_requirements_guide/2011-12-28/MAC-3_Sensitive/json). For more information about STIGs, visit the [STIG Viewer website](https://www.stigviewer.com/).
+
+## Demo
+
+You can watch a demo of the program in action here:
+[![asciicast](https://asciinema.org/a/XE996IdHwrNHKta63yD0ykInA.svg)](https://asciinema.org/a/XE996IdHwrNHKta63yD0ykInA)
+
+## Installation
+
+1. Make sure you have [Go](https://golang.org/) installed on your system.
+2. Clone this repository to your local machine.
+3. Navigate to the project directory and build the binary:
+
+```bash
+cd stig-viewer
+go build -o stgo ./main.go
+```
+
+## Usage
+
+### Listing Vulnerability IDs
+
+To list all Vulnerability IDs within the provided STIG data, use the `-file` flag for a local file or the `-url` flag for a remote URL:
+
+```bash
+./stgo -file stig.json 
+```
+
+Or:
+
+```bash
+./stgo -url https://www.example.com/stig.json 
+```
+
+## Searching for a Specific STIG Version
+
+To search for findings with a specific STIG version, use the -srg flag along with -file or -url:
+
+```bash
+./stgo -file ./stigs/example_app_stig.json -srg SRG-APP-000074
+```
+
+Or:
+
+```bash
+./main -url https://www.example.com/stig.json -srg SRG-APP-000074
+```
+
+### License
+
+This project is licensed under the MIT License. See the LICENSE file for details.
---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	fileFlag := flag.String("file", "", "Path to local JSON file")
+	urlFlag := flag.String("url", "", "URL to fetch the JSON data")
+	srgFlag := flag.String("srg", "", "STIG version to search for")
+	flag.Parse()
+
+	if *fileFlag == "" && *urlFlag == "" {
+		fmt.Println("Error: Neither file nor URL provided")
+		flag.PrintDefaults()
+		return
+	}
+
+	var data map[string]interface{}
+	var err error
+
+	if *fileFlag != "" {
+		data, err = readStigFromFile(*fileFlag)
+	} else {
+		data, err = readStigFromURL(*urlFlag)
+	}
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	findings := data["stig"].(map[string]interface{})["findings"].(map[string]interface{})
+
+	if *srgFlag == "" {
+		fmt.Println("List of IDs:")
+		for _, finding := range findings {
+			findingMap := finding.(map[string]interface{})
+			fmt.Println(findingMap["id"].(string))
+		}
+	} else {
+		matchingFindings := 0
+		for _, finding := range findings {
+			findingMap := finding.(map[string]interface{})
+			if findingMap["version"].(string) == *srgFlag {
+				matchingFindings++
+				fmt.Println("\033[4;36mSRG:\033[0m")
+				fmt.Println(findingMap["version"].(string))
+				fmt.Println("\033[4;36mSEVERITY:\033[0m")
+				fmt.Println(findingMap["severity"].(string))
+				fmt.Println("\033[4;36mTITLE:\033[0m")
+				fmt.Println(findingMap["title"].(string))
+				fmt.Println("\033[4;36mDESCRIPTION:\033[0m")
+				fmt.Println(findingMap["description"].(string))
+				fmt.Println("")
+			}
+		}
+
+		if matchingFindings == 0 {
+			fmt.Printf("No findings with version '%s' found.\n", *srgFlag)
+		}
+	}
+}
+
+func readStigFromFile(file string) (map[string]interface{}, error) {
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	var result map[string]interface{}
+	err = json.Unmarshal(data, &result)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func readStigFromURL(url string) (map[string]interface{}, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	var result map[string]interface{}
+	err = json.Unmarshal(data, &result)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
